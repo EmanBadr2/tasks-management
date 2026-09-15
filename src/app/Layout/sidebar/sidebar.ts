33ceb0f6b0a3f184f98e4384b85031e006adbec0
emanbadr <amonaelshafey252@gmail.com>
2026-09-16 02:06:59 +0300

    style: update sidebar collapse

--- src/app/Layout/sidebar/sidebar.ts
+++ src/app/Layout/sidebar/sidebar.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-sidebar',
   styleUrl: './sidebar.scss',
   templateUrl: './sidebar.html',
 })
-export class Sidebar {}
+export class Sidebar {
+
+   isCollapse = signal(false)
+   
+  collapseSidebar(){
+    this.isCollapse.update( value => !value)
+    console.log(this.isCollapse());
+    
+   }
+   logout(){
+    console.log('logout');
+    
+   }
+
+   
+}
