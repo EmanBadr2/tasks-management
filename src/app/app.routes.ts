460714fa9cb7c490b1d3a8c265993a00c9b0d2e8
emanbadr <amonaelshafey252@gmail.com>
2026-09-17 22:03:37 +0300

    refactor: handle layout routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
 
 
 export const routes: Routes = [
@@ -18,6 +19,7 @@ export const routes: Routes = [
 
     {
     path: 'MainLayout',
+    canActivate : [authGuard] ,
   loadChildren: () =>
       import( './Layout/layout-routes').then(
         m => m.LayoutRoutes
--- src/app/features/projects/projects.routing.ts
+++ src/app/features/projects/projects.routing.ts
@@ -2,6 +2,33 @@ import { Routes } from '@angular/router';
 
  export const ProjectsRoute : Routes = [
   
+          {
+            path:'' ,
+            redirectTo : 'Projects' ,
+            pathMatch : 'full' ,
+          },
 
-
- ]
+           { path : 'Projects' , loadComponent : () =>
+        import( './components/projects/projects').then(
+          m => m.Projects
+        ), } ,
+    {  
+          path: 'ProjectsList' ,
+          loadComponent : ()=>
+            import('./components/projects-list/projects-list').then(
+              m=>m.ProjectsList
+             ),
+        } ,
+         {  
+          path: 'AddEditProjects' ,
+          loadComponent : ()=>
+            import('./components/add-edit-projects/add-edit-projects').then(
+              m=>m.AddEditProjects
+             ),
+        } ,
+  
+ 
+  
+        ]
+  
+  
