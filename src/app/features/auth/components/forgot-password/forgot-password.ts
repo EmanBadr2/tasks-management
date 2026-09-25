354c764c92d29ab5f01aecd963706ac01fc34a4d
emanbadr <amonaelshafey252@gmail.com>
2026-09-25 19:50:21 +0300

    style : forget password UI , handle input style public

--- src/app/features/auth/components/forgot-password/forgot-password.ts
+++ src/app/features/auth/components/forgot-password/forgot-password.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-forgot-password',
   styleUrl: './forgot-password.scss',
   templateUrl: './forgot-password.html',
